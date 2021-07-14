7638c514f3b24a52e78f51748090ae5941267c33
Vladimir Ponomareko <p.v.v1313@gmail.com>
2021-07-14 19:10:17 +0300

    add new logic, molel and component

---
+++ src/app/shared/features.model.ts
@@ -0,0 +1,9 @@
+export class Languages {
+  public name: string;
+  public countOfFeatures: number;
+
+  constructor(name: string,  countOfFeatures: number) {
+    this.name = name;
+    this.countOfFeatures = countOfFeatures;
+  }
+}
--- src/app/wishes-list/wishes-list.component.ts
+++ src/app/wishes-list/wishes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Languages } from '../shared/features.model';
 
 @Component({
   selector: 'app-wishes-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wishes-list.component.scss']
 })
 export class WishesListComponent implements OnInit {
+  languages: Languages[] = [
+    new Languages('JavaScript', 10),
+    new Languages('NodeJs', 20)
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
